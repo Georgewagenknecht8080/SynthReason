aea4e84d613f434944f7860142dd13111fb65263
Georgewagenknecht8080 <50222052+Georgewagenknecht8080@users.noreply.github.com>
2019-05-11 19:07:03 +1000

    Strategic enigma.

--- SynthReason.js
+++ SynthReason.js
@@ -28,15 +28,18 @@ function OnStart()
 function btn_OnTouch()
 {
 
+
+
+while(0==0){
 		var txt = app.ReadFile("/sdcard/philosophy.txt");
 		var sentence = txt.split(".");
 		var fmax = 5;
-for (var x = 0; x < 10; x++)
+for (var x  = 0; x < 10; x++)
 	{
 	
 	
 		
-		//fmax = Math.floor(Math.random() * (7)) + 0;
+		fmax = Math.floor(Math.random() * (7)) + 0;
 
 		var noun = app.ReadFile("/sdcard/noun.txt");
 		var dic = app.ReadFile("/sdcard/words.txt");
@@ -48,78 +51,34 @@ for (var x = 0; x < 10; x++)
 			var words = sentencestr.split(" ");
 			var b = Math.floor(Math.random() * (words.length - 1)) + 0;
 			if (words[b - 1] !== undefined && words[b] !== undefined && words[b + 1] !== undefined && words[b + 1].length > 5 ){
-				if (dic.indexOf(words[b - 1]) > -1 && dic.indexOf(words[b]) > -1 && dic.indexOf(words[b + 1]) > -1){
-					outputprep = words[b - 1] + " " + words[b] + " " + words[b + 1] + " ";			
-			
-					if (outputprep.indexOf("ideology") > -1 || outputprep.indexOf("knowledge") > -1 || outputprep.indexOf("debate") > -1 || outputprep.indexOf("language") > -1 || outputprep.indexOf("voting") > -1 ){
-					txt = app.ReadFile("/sdcard/politics.txt");
-					sentence = txt.split(".");
-							fmax = Math.floor(Math.random() * (20)) + 10;
-					break;
-					}
-					
-					if (outputprep.indexOf("religion") > -1 || outputprep.indexOf("spiritual") > -1 || outputprep.indexOf("believe") > -1 || outputprep.indexOf("belief") > -1 || outputprep.indexOf("spirit") > -1  || outputprep.indexOf("Spirit") > -1 ){
-					txt = app.ReadFile("/sdcard/zen.txt");	
-					sentence = txt.split(".");
-					fmax = Math.floor(Math.random() * (10)) + 3;
-					break;
-					}
-			
-							if (outputprep.indexOf("medicine") > -1 || outputprep.indexOf("disease") > -1 || outputprep.indexOf("ill") > -1 || outputprep.indexOf("health") > -1 || outputprep.indexOf("sick") > -1){
-					txt = app.ReadFile("/sdcard/medicine.txt");
-					sentence = txt.split(".");
-					fmax = Math.floor(Math.random() * (20)) + 10;
-					break;
-					}	
-			
-							if (outputprep.indexOf("destruction") > -1 || outputprep.indexOf("violence") > -1 || outputprep.indexOf("murder") > -1 || outputprep.indexOf("ethics") > -1 || outputprep.indexOf("kill") > -1 || outputprep.indexOf("death") > -1 || outputprep.indexOf("attack") > -1){
-					txt = app.ReadFile("/sdcard/ethics.txt");
-					sentence = txt.split(".");
-					fmax = Math.floor(Math.random() * (5)) + 0;
-					break;
-					}	
+				if (dic.indexOf(words[b - 2]) > -1 && dic.indexOf(words[b -1]) > -1
+					&& dic.indexOf(words[b]) > -1 && dic.indexOf(words[b + 1]) > -1 && noun.indexOf(words[b]) > -1){
+						outputprep = words[b - 1] + " " + words[b] + " " + words[b + 1] + " ";			
+			var popup = 1;
 		
-							if (outputprep.indexOf("science") > -1 || outputprep.indexOf("design") > -1 || outputprep.indexOf("think") > -1 || outputprep.indexOf("create") > -1 || outputprep.indexOf("model") > -1){
-					txt = app.ReadFile("/sdcard/science.txt");
-					sentence = txt.split(".");
-				fmax = Math.floor(Math.random() * (20)) + 10;	
-					break;
-					}
-			
-							if (outputprep.indexOf("technology") > -1 || outputprep.indexOf("build") > -1 || outputprep.indexOf("create") > -1 || outputprep.indexOf("make") > -1){
-					txt = app.ReadFile("/sdcard/tech.txt");
-					sentence = txt.split(".");
-						fmax = Math.floor(Math.random() * (20)) + 10;
-					break;
-					}
+
+
+
 	
-							if (outputprep.indexOf("philosophy") > -1 || outputprep.indexOf("thought") > -1 || outputprep.indexOf("reason") > -1 || outputprep.indexOf("idea") > -1 || outputprep.indexOf("theory") > -1  || outputprep.indexOf("living") > -1   || outputprep.indexOf("idea") > -1 ){
-					txt = app.ReadFile("/sdcard/philosophy.txt");
+					if (outputprep.indexOf("system") > -1 || outputprep.indexOf("knowledge") > -1 || outputprep.indexOf("debate") > -1 || outputprep.indexOf("language") > -1 || outputprep.indexOf("model") > -1  || outputprep.indexOf("experience") > -1 ){
+					txt = app.ReadFile("/sdcard/cybernetics.txt");
 					sentence = txt.split(".");
-		     	fmax = Math.floor(Math.random() * (30)) + 10;
-					break;
-					}
-	
-	
-							if (outputprep.indexOf("business") > -1 || outputprep.indexOf("work") > -1 || outputprep.indexOf("money") > -1 || outputprep.indexOf("leader") > -1){
-					txt = app.ReadFile("/sdcard/business.txt");
-					sentence = txt.split(".");
-					fmax = Math.floor(Math.random() * (15)) + 10;
-					break;
-					}
-		
-							if (outputprep.indexOf("society") > -1 || outputprep.indexOf("environment") > -1 || outputprep.indexOf("people") > -1 || outputprep.indexOf("life") > -1 || outputprep.indexOf("group") > -1 || outputprep.indexOf("society") > -1|| outputprep.indexOf("language ") > -1){
-					txt = app.ReadFile("/sdcard/envsoc.txt");
-					sentence = txt.split(".");
-					fmax = Math.floor(Math.random() * (25)) + 10;
+							fmax = Math.floor(Math.random() * (15)) + 5;
+							
+									if (popup == 1){
+							app.ShowPopup( "cybernetic" );
+							}
 					break;
 					}
 					
 					
-							if ( outputprep.indexOf("strategy") > -1 || outputprep.indexOf("maze") > -1 || outputprep.indexOf("system") > -1 || outputprep.indexOf("intelligence") > -1 || outputprep.indexOf("process") > -1){
+							if ( outputprep.indexOf("mechanisms") > -1  || outputprep.indexOf("synthetic") > -1 || outputprep.indexOf("strategy") > -1 || outputprep.indexOf("maze") > -1  || outputprep.indexOf("intelligence") > -1 || outputprep.indexOf("process") > -1){
 					txt = app.ReadFile("/sdcard/artificialintelligence.txt");					
 					sentence = txt.split(".");
 					fmax = Math.floor(Math.random() * (10)) + 5;
+							if (popup == 1){
+							app.ShowPopup( "artificial intelligence" );
+							}
 					break;
 					}
 					
@@ -145,5 +104,6 @@ for (var x = 0; x < 10; x++)
 		//Resources generation
 		app.WriteFile( "/sdcard/helloworld.txt", output);	
 		edt.SetText(output);
+	}
 	}
 }
