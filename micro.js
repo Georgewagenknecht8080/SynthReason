c03730d24215b24448a1fe84c8b04acb770778ee
Georgewagenknecht8080 <50222052+Georgewagenknecht8080@users.noreply.github.com>
2019-06-27 16:54:52 +1000

    Add files via upload

--- micro.js
+++ micro.js
@@ -12,12 +12,14 @@ function OnStart()
 }
 function btn_OnTouch()
 {
-	var bootloader = app.ReadFile("/sdcard/artificialintelligence.txt");
+	var bootloader = app.ReadFile("/sdcard/philosophy.txt");
 	var sentencea = bootloader.split(".");
 	var aa = Math.floor(Math.random() * (sentencea.length)) + 0;
 	var sentencestra = sentencea[aa];
-	for (var count = 0; count < 25;count++ )
+	for (var count = 0; count < 250;count++ )
 	{
+		var aa = Math.floor(Math.random() * (sentencea.length)) + 0;
+	var sentencestra = sentencea[aa];
 		var dic = app.ReadFile("/sdcard/words.txt");
 		var words = sentencestra.split(" ");
 		var x = Math.floor(Math.random() * (words.length)) + 0;
@@ -33,13 +35,13 @@ function btn_OnTouch()
 				var words2 = sentencestra.split(" ");
 				var y = Math.floor(Math.random() * (words2.length)) + 0;
 			}
-			if (words[x] !== undefined && words2[y + 1] !== undefined)
+			if (words[x] !== undefined && words2[y + 1] !== undefined && words[x + 2] !== undefined && words2[y + 3] !== undefined && words[x + 4] !== undefined && words2[y + 5] !== undefined)
 			{
 				if (output.indexOf(words[x]) == -1 && output.indexOf(words2[y + 1]) == -1)
 				{
-					output += words[x] + " " + words2[y + 1] + " ";
+					output += words[x] + " " + words2[y + 1] + " " + words[x + 2] + " " + words2[y+3] + " " + words[x + 4] + " " + words[y + 5] + " ";
 					edt.SetText( output );
-			
+			app.SetClipboardText(output );
 				}
 			}
 		}
